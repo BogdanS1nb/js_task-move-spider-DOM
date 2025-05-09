04a1d3396ca0e11ac42f6218e8d2fee8fbd31bde
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-05-09 20:22:57 +0300

    add sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  // Курсор відносно стіни
+  const offsetX = e.clientX - wallRect.left;
+  const offsetY = e.clientY - wallRect.top;
+
+  const clampedLeft = Math.min(
+    Math.max(0, offsetX - spiderWidth / 2),
+    wall.clientWidth - spiderWidth,
+  );
+
+  const clampedTop = Math.min(
+    Math.max(0, offsetY - spiderHeight / 2),
+    wall.clientHeight - spiderHeight,
+  );
+
+  spider.style.left = `${clampedLeft}px`;
+  spider.style.top = `${clampedTop}px`;
 });
